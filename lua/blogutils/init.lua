641af66983e96f4b3067c48d9831efd68b31ef70
Christopher Hannah <me@chrishannah.me>
2024-01-08 00:34:56 +0000

    feat: Add functions to format text as a title or slug, and to generate frontmatter from a given title

---
+++ lua/blogutils/init.lua
@@ -0,0 +1,52 @@
+local M = {}
+
+M.formatTitle = function(input)
+    return M.formatText(input, "apTitle")
+end
+
+M.formatSlug = function(input)
+    return M.formatText(input, "slug")
+end
+
+M.formatText = function(input, format)
+    local Job = require("plenary.job")
+
+    local output = input
+    Job:new({
+        command = "textcase",
+        args = {
+            "--format", format,
+            "--input", input,
+        },
+        cwd = "/usr/bin",
+        on_stdout = function(_, data, _)
+            output = data
+        end,
+    }):sync()
+
+    return output
+end
+
+M.generateFrontMatter = function()
+    -- get first row of file as input
+    local input = vim.api.nvim_buf_get_lines(0, 0, 1, true)[1] or "Example Title"
+
+    -- prepare data
+    local title = M.formatTitle({ input = input })
+    local slug = M.formatSlug({ input = input })
+    local date = vim.fn.strftime("%Y-%m-%dT%H-%M-%S")
+
+    -- write to top of buffer
+    vim.api.nvim_buf_set_lines(0, 0, 1, true, {
+        '---',
+        'title: ' .. title,
+        'slug: ' .. slug,
+        'date: ' .. date,
+        'categories: []',
+        '---'
+    })
+end
+
+return M
+
+
